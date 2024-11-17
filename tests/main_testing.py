a563138d7506b2754fe4222dda46de6b594de53e
JackCreatorOfCode <jhawkes2@students.kennesaw.edu>
2024-11-17 15:30:11 -0500

    Adding second function definition

--- tests/main_testing.py
+++ tests/main_testing.py
@@ -4,3 +4,11 @@ def add_task(pet_name, task_type, date, time):
     task = ("pet_name", pet_name, "task_type", task_type, "date", date, "time", time)
     print("Task added: " + str(task))
     return task
+
+def view_schedule(schedule):
+    """Display all tasks in the schedule."""
+    if len(schedule) == 0:
+        print("No tasks scheduled.")
+    else:
+        for task in schedule:
+            print(task[1] + " - " + task[3] + " on " + task[5] + " at " + task[7])
